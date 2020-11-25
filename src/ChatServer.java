72fc17d8a02b2b29e0c8511f0fbc9e5334d81a62
sanchesaline6 <sanches.aline6@gmail.com>
2020-11-25 13:17:23 -0300

    last updates

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,27 +1,60 @@
-import java.io.*;
-import java.net.PortUnreachableException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ChatServer {
-
-    //evitar portas 80, 22, 21
     public static final int PORT = 4000;
     private ServerSocket serverSocket;
-    private BufferedReader in;
+    private final List<ClientSocket> clients = new LinkedList<>();
 
     public void start() throws IOException {
         serverSocket = new ServerSocket(PORT);
-        System.out.println("Servidor iniciado na porta: " + PORT);
+        System.out.println("Servidor iniciado na porta " + PORT);
         clientConnectionLoop();
     }
+
     private void clientConnectionLoop() throws IOException {
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Cliente " + clientSocket.getRemoteSocketAddress() + " conectou");
-            this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String msg = in.readLine();
-            System.out.println("Mensagem recebida do cliente: " + clientSocket.getRemoteSocketAddress() + ": " + msg);
+        while(true) {
+            ClientSocket clientSocket = new ClientSocket(serverSocket.accept());
+            clients.add(clientSocket);
+            new Thread(() -> clientMessageLoop(clientSocket)).start();
+
+        }
+    }
+
+    private void clientMessageLoop(ClientSocket clientSocket) {
+        String msg;
+        try {
+            while((msg = clientSocket.getMessage()) != null){
+                if("sair".equalsIgnoreCase(msg))
+                    return;
+                System.out.printf("Mensagem recebida do cliente %s: %s\n",
+                        clientSocket.getRemoteSocketAddress(),
+                        msg);
+
+                sendMessageToAll(clientSocket, msg);
+            }
+        } finally {
+            clientSocket.close();
+        }
+    }
+
+    private void sendMessageToAll(ClientSocket sender, String msg) {
+       Iterator<ClientSocket> iterator = clients.iterator();
+        while (iterator.hasNext()) {
+            ClientSocket clientSocket = iterator.next();
+            if(!sender.equals(clientSocket)){
+                if(!clientSocket.sendMessage("cliente" + sender.getRemoteSocketAddress() + ": " + msg)){
+                    iterator.remove();
+                }
+
+            }
+
         }
     }
     public static void main(String[] args) {
